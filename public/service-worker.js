6e680ed76bb2400dfaa67ed0d53d3fa822dcbb5e
Diana Koeswanto <d.koeswanto@gmail.com>
2021-07-11 17:40:54 +1000

    Fixed path to files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,14 @@
-const CACHED_FILES = [
-	"/",
-	"/index.html",
-	"/db.js",
-	"/index.js",
-	"/styles.css",
-	"/icons/icon-192x192.png",
-	"/icons/icon-512x512.png",
-	"/manifest.json"
+const FILES_TO_CACHE = [
+	"./",
+	"./index.html",
+	"./db.js",
+	"./index.js",
+	"./styles.css",
+	"./icons/icon-192x192.png",
+	"./icons/icon-512x512.png",
+	"./manifest.webmanifest"
   ];
-
-  const CACHE_NAME = "static-cache";
-  const DATA_CACHE_NAME = "data-cache";
-
+  
 // install
 self.addEventListener("install", (evt) => {
 	evt.waitUntil(
